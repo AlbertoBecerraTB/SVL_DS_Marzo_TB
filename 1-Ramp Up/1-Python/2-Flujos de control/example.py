8997ce0d1fe473411dc073c22f460ce5752a5c01
Alberto Becerra <alberto.becerra@thebridgeschool.es>
2022-03-28 14:04:52 +0200

    Clase de bucles, comentarios y script de prueba para debugger

---
+++ 1-Ramp Up/1-Python/2-Flujos de control/example.py
@@ -0,0 +1,15 @@
+# for i in range(10):
+#     j = i ** 2
+#     print("Hola Mundo")
+
+# print("Adios Mundo")
+
+# k = 18
+
+# print("jajajjaa")
+
+
+i = 0
+while i <= 10:
+    print("Hola Mundo Numero", i)
+    i = i + 1
